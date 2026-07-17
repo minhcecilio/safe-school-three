9c744e105430bbbf023907992911d9d1daf51ca8
DuLongytb <minhtn1@gmail.com>
2026-07-17 17:04:51 +0700

    add2

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBNUUlJXVNFACeRYsFpKy5sfyLOYBg8HN0",
+    authDomain: "shieldup-f9f03.firebaseapp.com",
+    projectId: "shieldup-f9f03",
+    storageBucket: "shieldup-f9f03.firebasestorage.app",
+    messagingSenderId: "547128386315",
+    appId: "1:547128386315:web:238e10ae2498912d90b80e",
+    measurementId: "G-NK8MGQMMCS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
